a412036e4cbb1ab9bca25a6c59c10dc4e6387025
Samy Touabi <stoua026@uottawa.ca>
2025-01-05 02:01:05 -0500

    added functional contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const [name, setName] = useState("");
@@ -10,22 +11,47 @@ const Contact = () => {
   const history = useHistory();
 
   const handleSubmit = () => {
-    const userMessage = { name, email, message };
+    // e.preventDefault() // Not sure if I need this
+
+    const serviceId = "service_fbk5nvr";
+    const templateId = "template_kmg7w1a";
+    const publicKey = "aiS3EMuigPfAk1yPD";
+
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: "Administrator",
+      message: message,
+    };
 
     setIsPending(true);
 
     // Simulate backend processing
-    console.log("Message sent:", userMessage);
 
-    setTimeout(() => {
-      setIsPending(false);
-      alert("Your message has been sent!");
-      setName("");
-      setEmail("");
-      setMessage("");
-      setShowConfirm(false);
-      history.push("/");
-    }, 1000);
+    emailjs
+      .send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        console.log("Email Message sent:", response);
+        setName("");
+        setEmail("");
+        setMessage("");
+      })
+      .catch((error) => {
+        console.error("Error sending email:", error);
+      });
+    setIsPending(false);
+    setShowConfirm(false);
+    history.push("/");
+
+    // setTimeout(() => {
+    //   setIsPending(false);
+    //   alert("Your message has been sent!");
+    //   setName("");
+    //   setEmail("");
+    //   setMessage("");
+    //   setShowConfirm(false);
+    //   history.push("/");
+    // }, 1000);
   };
 
   const handleFormSubmit = (e) => {
