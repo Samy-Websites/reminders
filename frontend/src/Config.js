3beb5f25dce2c30f7b7817d71ce84a6ad12d4fcd
Samy Touabi <stoua026@uottawa.ca>
2025-01-07 00:21:43 -0500

    updated DB urls for front end to point to render backend

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,5 +1,5 @@
-const REMINDERS_URL = "http://localhost:5000/api/reminders"; // Base URL for reminders
-const SUBSCRIBERS_URL = "http://localhost:5000/subscribe"; // Subscriptions endpoint
+const REMINDERS_URL = "https://reminders-backend.onrender.com/api/reminders"; // Base URL for reminders
+const SUBSCRIBERS_URL = "https://reminders-backend.onrender.com/subscribe"; // Subscriptions endpoint
 
 export default REMINDERS_URL; // Default export
 export { SUBSCRIBERS_URL }; // Named export
