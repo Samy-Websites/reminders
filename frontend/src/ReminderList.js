d277f5379243bb2203cf676354d32ce59d6ba157
Samy Touabi <stoua026@uottawa.ca>
2025-02-04 14:31:56 -0500

    sorted chronologically the reminders list

--- frontend/src/ReminderList.js
+++ frontend/src/ReminderList.js
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
 
 const ReminderList = ({ reminders, title }) => {
+  // Sort reminders by date
+  const sortedReminders = [...reminders].sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+
   return (
     <div className="reminder-list">
       <h1> {title} </h1>
       <hr />
-      {reminders.map((reminder) => (
+      {sortedReminders.map((reminder) => (
         <div className="reminder-preview" key={reminder._id}>
           <Link to={`/reminders/${reminder._id}`}>
             <h2>{reminder.title}</h2>
